d8e62f753a12440b332e552b28e50500d5026d9d
wd <lucasalvesbispo2@gmail.com>
2026-01-08 18:53:47 +0000

    mixin bullshit

--- src/main/java/com/wdiscute/starchaeology/Starchaeology.java
+++ src/main/java/com/wdiscute/starchaeology/Starchaeology.java
@@ -10,6 +10,7 @@ import net.minecraft.resources.ResourceLocation;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.config.ModConfig;
 import org.slf4j.Logger;
 
 @Mod(Starchaeology.MOD_ID)
@@ -47,8 +48,8 @@ public class Starchaeology
 //        ModTackleSkins.register(modEventBus);
 //        ModCriterionTriggers.register(modEventBus);
 
-        //modContainer.registerConfig(ModConfig.Type.CLIENT, Config.SPEC);
-        //modContainer.registerConfig(ModConfig.Type.SERVER, Config.SPEC_SERVER);
+        modContainer.registerConfig(ModConfig.Type.CLIENT, Config.SPEC);
+        modContainer.registerConfig(ModConfig.Type.SERVER, Config.SPEC_SERVER);
     }
 
 
